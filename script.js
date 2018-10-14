e86ea332fc5124edf9d949c36073d0f2089d21c3
itsmadhusudhan <madhusudhan1863@gmail.com>
2018-10-14 20:36:00 +0530

    added more styles and refactoring and added remote git repo origin

--- script.js
+++ script.js
@@ -1,48 +1,49 @@
 const url = "https://itunes.apple.com/search?term=";
-const url2="https://itunes.apple.com/lookup?id="
+const url2 = "https://itunes.apple.com/lookup?id="
 
 let data = [];
 const search = document.querySelector('.i-search');
 const results = document.querySelector('.i-results');
-const favouriteList=[];
 
+const favouriteList = [];
+let loading = true;
 const getSearchResults = (e) => {
   const value = e.target.value;
-  value && fetch(url + value + "+&media=music&entity=song")
+  value ? fetch(url + value + "&media=music&entity=song")
     .then(res => res.json())
     .then(tunes => {
-      data.push(tunes.results)
-      console.log(data)
       processResults(tunes)
+    })
+    .catch(err => console.log(err)) : results.innerHTML = "";
+}
+
+const getTrackDetails = (trackId) => {
+  console.log(trackId)
+  trackId && fetch(url2 + trackId)
+    .then(res => res.json())
+    .then(details => {
+      ProcessTuneDetails(details)
     })
     .catch(err => console.log(err))
 }
 
-const getTrackDetails=(trackId)=>{
-  console.log(trackId)
-  trackId && fetch(url2+trackId)
-  .then(res => res.json())
-  .then(details => {
-    ProcessTuneDetails(details)
-  })
-  .catch(err => console.log(err))
-}
-
 const createTuneCard = (results) => {
-  return results.map(result=>{
+  return results.map(result => {
     return `
     <div class="i-tune__card"  data-track-id=${result.trackId}>
-      <h2 class="i-tune__card--title">${result.trackCensoredName}</h2>
-      <p class="i-tune__card--trackid">${result.trackId}</p>
-      <p class="i-tune__card--album">${result.collectionName}</p>
-      <p class="i-tune__card--album">${milliSecondsToMinutes(result.trackTimeMillis)}</p>
+    <img src=${result.artworkUrl100} class="i-tune__card--image"/>
+    <div class="i-tune__card--details" data-track-id=${result.trackId}>
+      <h3 class="i-tune__card--title" data-track-id=${result.trackId}>${result.trackCensoredName}</h3>
+      <p class="i-tune__card--album" data-track-id=${result.trackId}>${result.collectionName}</p>
+      <p class="i-tune__card--album" data-track-id=${result.trackId}>${milliSecondsToMinutes(result.trackTimeMillis)}</p>
+      </div>
     </div>`
   }).join('');
 
 }
 
-const createTuneDetailsModal=(results)=>{
-  return results.map(result=>{
+const createTuneDetailsModal = (results) => {
+  return results.map(result => {
     return `
     <div class="i-tune__card--modal">
     <h2 class="i-tune__card--title">${result.trackCensoredName}</h2>
@@ -51,28 +52,28 @@ const createTuneDetailsModal=(results)=>{
   }).join('');
 }
 
-const handleClick=function(e){
-  console.log(e.currentTarget)
-  const trackId=e.target.getAttribute('data-track-id');
+const handleClick = function (e) {
+  const trackId = e.target.getAttribute('data-track-id');
   getTrackDetails(trackId);
 }
 
 const processResults = data => {
   const html = createTuneCard(data.results);
-  results.innerHTML=html;
-  const cards=document.querySelectorAll('.i-tune__card');
-  cards.forEach(card=>card.addEventListener('click',handleClick))
+  results.innerHTML = html;
+  const cards = document.querySelectorAll('.i-tune__card');
+  cards.forEach(card => card.addEventListener('click', handleClick))
 }
 
-const ProcessTuneDetails=data=>{
+const ProcessTuneDetails = data => {
   console.log(data)
   // const html=createTuneDetailsModal(data.results);
 }
 
-const milliSecondsToMinutes=(milliSeconds)=>{
-  return Math.floor(milliSeconds/60000)+"m"
+const milliSecondsToMinutes = (milliSeconds) => {
+  return Math.floor(milliSeconds / 60000) + "m"
 }
 
 
 // console.log(data)
-search.addEventListener('keyup', getSearchResults)
+search.addEventListener('keyup', getSearchResults);
+
