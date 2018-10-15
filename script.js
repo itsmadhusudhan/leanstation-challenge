86b2fe70f77c6bf846f8aaa23f0be00e3135a1ea
itsmadhusudhan <madhusudhan1863@gmail.com>
2018-10-15 19:30:06 +0530

    added functionality to persist favourite list in local storage and refactored code to use IIFE to avoid values in global scope and also modal function is improved

--- script.js
+++ script.js
@@ -1,179 +1,196 @@
-const url = 'https://itunes.apple.com/search?term=';
-const url2 = 'https://itunes.apple.com/lookup?id=';
+(function() {
+  const url = "https://itunes.apple.com/search?term=";
+  const url2 = "https://itunes.apple.com/lookup?id=";
 
-/* query and keep dom nodes of search and results node */
-const search = document.querySelector('.i-search');
-const results = document.querySelector('.i-results');
+  /* query and keep dom nodes of search and results node */
+  const search = document.querySelector(".i-search");
+  const results = document.querySelector(".i-results");
+  const modal = document.querySelector(".i-tune__modal");
+  const modalContent = document.querySelector(".i-tune__modal--content");
+  const list=document.querySelector('.i-favourite__list');
 
-const btn = document.querySelector('.btn');
+  // empty array to store favourtite track list
+  let favouriteList = JSON.parse(localStorage.getItem('favourites'))||[];
+  console.log(favouriteList)
 
-const modal = document.querySelector('.i-tune__modal');
+  /**
+   * @param {number} milliSeconds
+   * @returns {string}
+   */
+  const milliSecondsToMinutes = milliSeconds =>
+    `${Math.floor(milliSeconds / 60000)}m`;
 
-const modalContent = document.querySelector('.i-tune__modal--content');
-
-const modalClose = document.querySelector('.i-modal__close');
-
-// empty array to store favourtite track list
-let favouriteList = [];
-
-/**
- * @param {number} milliSeconds
- * @returns {string}
- */
-const milliSecondsToMinutes = milliSeconds =>
-	`${Math.floor(milliSeconds / 60000)}m`;
-
-/**
- *
- * @param {Object[]} cardResults
- * @returns {String}
- */
-const createTuneCard = cardResults =>
-	cardResults
-		.map(
-			result => `
+  /**
+   *
+   * @param {Object[]} cardResults
+   * @returns {String}
+   */
+  const createTuneCard = cardResults =>
+    cardResults
+      .map(
+        result => `
     <div class="i-tune__card"  data-track-id=${result.trackId}>
     <img src=${result.artworkUrl100} class="i-tune__card--image"/>
     <div class="i-tune__card--details" data-track-id=${result.trackId}>
       <h3 class="i-tune__card--title" data-track-id=${result.trackId}>${
-				result.trackCensoredName
-			}</h3>
+          result.trackCensoredName
+        }</h3>
       <p class="i-tune__card--album" data-track-id=${result.trackId}>${
-				result.collectionName
-			}</p>
+          result.collectionName
+        }</p>
       <p class="i-tune__card--album" data-track-id=${
-				result.trackId
-			}>${milliSecondsToMinutes(result.trackTimeMillis)}</p>
+        result.trackId
+      }>${milliSecondsToMinutes(result.trackTimeMillis)}</p>
       </div>
-      <p class="i-tune__card--favourite"  data-track-id=${
-				result.trackId
-			}>add to favourite</p>
+      	<img src="./images/like.svg" class="i-tune__card--favourite"  data-track-id=${
+          result.trackId
+        }></img>
     </div>`
-		)
-		.join('');
+      )
+      .join("");
 
-/**
- *
- * @param {Object[]} details
- * @return {String}
- */
-const createTuneDetailsModal = details =>
-	details
-		.map(
-			result => `
+  /**
+   *
+   * @param {Object[]} details
+   * @return {String}
+   */
+  const createTuneDetailsModal = details =>
+    details
+      .map(
+        result => `
     <div class="i-tune__card--modal">
 <img src=${result.artworkUrl100} class="i-tune__card--image"/>
 <div class="i-tune__card--details" data-track-id=${result.trackId}>
 	<h3 class="i-tune__card--title" data-track-id=${result.trackId}>${
-				result.trackCensoredName
-			}</h3>
+          result.trackCensoredName
+        }</h3>
 	<p class="i-tune__card--album" data-track-id=${result.trackId}>${
-				result.collectionName
-			}</p>
+          result.collectionName
+        }</p>
 	<p class="i-tune__card--album" data-track-id=${
-		result.trackId
-	}>${milliSecondsToMinutes(result.trackTimeMillis)}</p>
+    result.trackId
+  }>${milliSecondsToMinutes(result.trackTimeMillis)}</p>
 	</div>
-	<p class="i-tune__card--favourite"  data-track-id=${
-		result.trackId
-	}>add to favourite</p>
+	<img src="./images/like.svg" class="i-tune__card--favourite"  data-track-id=${
+    result.trackId
+  }></img>
     </div>`
-		)
-		.join('');
+      )
+      .join("");
 
-/**
- *
- * @param {number[]} list
- * @param {number} id
- * @returns {number[]}
- */
-const filterFavourites = (list, id) => {
-	console.log(list.includes(id));
-	return list.includes(id) ? list.filter(track => track !== id) : [...list, id];
-};
+  /**
+   *
+   * @param {number[]} list
+   * @param {number} id
+   * @returns {number[]}
+   */
+  const filterFavourites = (list, id) => {
+    return list.includes(id)
+      ? list.filter(track => track !== id)
+      : [...list, id];
+  };
 
-/**
- *  * @param {*} e
- */
-const handleFavourites = e => {
-	const trackId = e.target.getAttribute('data-track-id');
-	favouriteList = filterFavourites(favouriteList, trackId);
-};
+  /**
+   *  * @param {*} e
+   */
+  const handleFavourites = e => {
+    const trackId = e.target.getAttribute("data-track-id");
+    favouriteList = filterFavourites(favouriteList, trackId);
+    localStorage.setItem('favourites', JSON.stringify(favouriteList));
+    console.log(favouriteList);
+  };
 
-/**
- *
- * @param {*} data
- */
-const processResults = data => {
-	const html = createTuneCard(data.results);
-	results.innerHTML = html;
-	const cards = document.querySelectorAll('.i-tune__card');
-	cards.forEach(card => card.addEventListener('click', handleClick));
-	const favouriteBtn = document.querySelectorAll('.i-tune__card--favourite');
-	favouriteBtn.forEach(btn => btn.addEventListener('click', handleFavourites));
-};
+  /**
+   *
+   * @param {*} data
+   */
+  const processResults = data => {
+    const html = createTuneCard(data.results);
+    results.innerHTML = html;
+    const cards = document.querySelectorAll(".i-tune__card");
+    cards.forEach(card => card.addEventListener("click", handleClick));
+    const favouriteBtn = document.querySelectorAll(".i-tune__card--favourite");
+    favouriteBtn.forEach(btn =>
+      btn.addEventListener("click", handleFavourites)
+    );
+  };
 
-/**
- *
- * @param {*} data
- */
-const ProcessTuneDetails = data => {
-	console.log(data);
-	const html = createTuneDetailsModal(data.results);
-	modalContent.innerHTML = html;
-};
+  /**
+   *
+   * @param {*} data
+   */
+  const ProcessTuneDetails = data => {
+    console.log(data);
+    const html = createTuneDetailsModal(data.results);
+    modalContent.innerHTML = html;
+    const modalClose = document.querySelector(".i-modal__close");
+    modalClose.addEventListener("click", closeModal);
+  };
 
-/**
- * * @param {*} e
- */
-const getSearchResults = e => {
-	const value = e.target.value;
-	value
-		? fetch(`${url + value}&media=music&entity=song`)
-				.then(res => res.json())
-				.then(tunes => {
-					processResults(tunes);
-				})
-				.catch(err => console.log(err))
-		: (results.innerHTML = '');
-};
+  /**
+   * * @param {*} e
+   */
+  const getSearchResults = e => {
+    const value = e.target.value;
+    value
+      ? fetch(`${url + value}&media=music&entity=song`)
+          .then(res => res.json())
+          .then(tunes => {
+            processResults(tunes);
+          })
+          .catch(err => console.log(err))
+      : (results.innerHTML = "");
+  };
 
-/**
- *
- * @param {number} trackId
- */
-const getTrackDetails = trackId => {
-	// console.log(trackId);
-	trackId &&
-		fetch(url2 + trackId)
-			.then(res => res.json())
-			.then(details => {
-				ProcessTuneDetails(details);
-			})
-			.catch(err => console.log(err));
-};
+  /**
+   *
+   * @param {number} trackId
+   */
+  const getTrackDetails = trackId => {
+    // console.log(trackId);
+    trackId &&
+      fetch(url2 + trackId)
+        .then(res => res.json())
+        .then(details => {
+          ProcessTuneDetails(details);
+        })
+        .catch(err => console.log(err));
+  };
 
-/**
- *
- * @param {*} e
- */
-const handleClick = e => {
-	const trackId = e.target.getAttribute('data-track-id');
-	getTrackDetails(trackId);
-	modal.classList.add('i-tune__modal--active');
-	modalContent.classList.add('i-tune__modal--content-active');
-};
+  const openModal = () => {
+    modal.classList.add("i-tune__modal--active");
+    modalContent.classList.add("i-tune__modal--content-active");
+  };
 
-search.addEventListener('keyup', getSearchResults);
+  const closeModal = () => {
+    modal.classList.remove("i-tune__modal--active");
+    modalContent.classList.remove("i-tune__modal--content-active");
+    modalContent.innerHTML = "";
+  };
 
-modalClose.addEventListener('click', () => {
-	modal.classList.remove('i-tune__modal--active');
-	modalContent.classList.remove('i-tune__modal--content-active');
-});
+  /**
+   *
+   * @param {*} e
+   */
+  const handleClick = e => {
+    const trackId = e.target.getAttribute("data-track-id");
+    // console.log(trackId);
+    let proceed =
+      !e.target.classList.contains("i-tune__card--favourite") &&
+      trackId !== null
+        ? true
+        : false;
+    proceed && getTrackDetails(trackId);
+    proceed && openModal();
+  };
 
-document.addEventListener('click', () => {
-	// modal.classList.contains('i-tune__modal--active') ?
-	// 	modal.classList.remove('i-tune__modal--active'):"";
-	// modalContent.classList.contains('i-tune__modal--content-active') ?
-	// 	modalContent.classList.remove('i-tune__modal--content-active'):"";
-});
+  const init = () => {
+    search.addEventListener("keyup", getSearchResults);
+    modal.addEventListener("click", closeModal);
+    list.addEventListener('click',()=>{
+      console.log("hello")
+    })
+  };
+
+  init();
+})();
